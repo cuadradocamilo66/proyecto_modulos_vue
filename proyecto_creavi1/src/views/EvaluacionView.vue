7bdb1b74ec55385ea3928b0ba09e9ecb747222a0
chenach <cuadradocamilo66@gmail.com>
2022-05-14 10:57:49 -0500

    botones actualizados

--- proyecto_creavi1/src/views/EvaluacionView.vue
+++ proyecto_creavi1/src/views/EvaluacionView.vue
@@ -1,5 +1,59 @@
 <template>
- <h1>EVALUACION MODULO</h1>
+  <v-container>
+        <h1>EVALUACION MODULO</h1>
+ <v-row
+    align="center"
+    justify="space-around"
+  >
+    <v-btn rounded
+    color="blanco">
+      Inicio
+    </v-btn>
+
+    <v-btn rounded
+    color="blanco"> 
+      Contenidos
+    </v-btn>
+
+    <v-btn rounded 
+    color="blanco">
+      Actividades
+    </v-btn>
+
+    <v-btn
+      rounded
+      color="verde"
+    >
+      Evaluación
+    </v-btn>
+
+    <v-btn
+      rounded 
+      color="blanco">
+      Bibliografía
+    </v-btn>
+  </v-row>
+
+
+
+         <v-col cols="12">
+             <v-img
+                :src="require('../assets/learning.svg')"
+                position="center"
+                class="my-4"
+                 width="400"
+             />
+            <v-img
+                :src="require('../assets/wave.svg')"
+                class="my-4"
+                contain
+                 max-width="1250"
+                 max-height="250"
+             />
+
+        </v-col>
+    </v-container>
+
 </template>
 
 <script>
