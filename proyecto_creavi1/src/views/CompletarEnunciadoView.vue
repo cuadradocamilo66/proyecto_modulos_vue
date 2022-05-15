701cc9d77009c18d3b889d04cf5a9195f0143771
chenach <cuadradocamilo66@gmail.com>
2022-05-15 10:56:14 -0500

    completar enunciado terminado

--- proyecto_creavi1/src/views/CompletarEnunciadoView.vue
+++ proyecto_creavi1/src/views/CompletarEnunciadoView.vue
@@ -16,6 +16,47 @@
                 Escriba el enunciado y seleccione las palabras a faltar:
           </h2>
       </div>
+           <div  style= "margin-left:100px; margin-top: 50px;">
+                <v-textarea xs2
+                 style="width:550px; "
+                     solo
+                    label="texto a completar"
+                ></v-textarea>
+        </div>
+        <div style="margin-top:0px">
+          <h2  style= "font-size:20px; 
+                    margin-left:80px;
+                    color: #FFFFFF;
+                    font-family: 'Roboto', sans-serif;">
+                Seleccione las palabras a ocultar:
+          </h2>
+      </div>
+
+       <div style="display:flex; margin-top:240px">
+
+        <div  style= "margin-left:100px; margin-top: 0px; ">
+            <v-btn
+              rounded 
+              color="error">
+              Atrás
+            </v-btn>
+        </div>
+         <div  style= "margin-left:300px; margin-top: 0px; ">
+            <v-btn
+              rounded 
+              color="primary">
+              Finalizar
+            </v-btn>
+        </div>
+        <div  style= "margin-left:300px; margin-top: 0px; ">
+            <v-btn
+              rounded 
+              color="warning">
+              Siguiente
+            </v-btn>
+        </div>
+    </div>
+
   </v-container>
 
 </template>
